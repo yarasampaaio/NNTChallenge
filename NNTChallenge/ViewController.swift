1e738ec92db09387e8fbca5b14b9eae095af9aff
yarasampaaio <yaravitoria16@gmail.com>
2022-08-29 17:47:30 -0300

    Initial Commit

---
+++ NNTChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NNTChallenge
+//
+//  Created by Yara Vitoria on 29/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NNTChallengeUITests/NNTChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NNTChallengeUITestsLaunchTests.swift
+//  NNTChallengeUITests
+//
+//  Created by Yara Vitoria on 29/08/22.
+//
+
+import XCTest
+
+class NNTChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
